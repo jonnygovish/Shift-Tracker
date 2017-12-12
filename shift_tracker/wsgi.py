73b5b243e75547a412e3560af61d5c459918c6a0
John <jonnygovish@gmail.com>
2017-12-12 19:59:11 +0300

    initial commit

---
+++ shift_tracker/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for shift_tracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shift_tracker.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
